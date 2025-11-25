8a92d64b82a5b4cbe09d27c3cb6c85ed7b19c15d
tobi <tobias@schwartz.earth>
2025-11-25 17:57:00 +0000

    Add a resources to wait for the VM getting started and generate files .ssh/config.d/vmID and inventory.ini with IPs of new VMs

--- devops-vm/terraform/main.tf
+++ devops-vm/terraform/main.tf
@@ -5,6 +5,11 @@ terraform {
       source  = "bpg/proxmox"
       version = "~> 0.70"
     }
+    # Only for temporary use. Can be removed, when waiting for IPs is done by pipeline.
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.11"
+    }
   }
 }
 
@@ -37,7 +42,7 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   }
   
   # Set to 'true', if the VM shall start automatically after 'terraform apply':
-  started = false
+  started = true
   on_boot = false
   
   # Agent
@@ -137,4 +142,80 @@ output "vm_ips" {
   value       = [for vm in proxmox_virtual_environment_vm.ubuntu_vm : 
     vm.started ? try(vm.ipv4_addresses[1][0], "Not available yet") : "VM not started"
   ]
+}
+
+# Wait 60 seconds after VM creation.
+# Only for temporary use. Can be removed, when waiting for IPs is done by pipeline.
+resource "time_sleep" "wait_for_agent" {
+  depends_on = [proxmox_virtual_environment_vm.ubuntu_vm]
+  
+  create_duration = "60s"
+  
+  triggers = {
+    # Will be generated newly, if VMs are created
+    vm_ids = join(",", [for vm in proxmox_virtual_environment_vm.ubuntu_vm : vm.id])
+  }
+}
+
+# SSH-Config: One file per VM
+resource "local_file" "ssh_config" {
+  depends_on = [time_sleep.wait_for_agent]
+
+  count = var.vm_count
+  
+  filename = pathexpand("~/.ssh/config.d/terraform-vm-${proxmox_virtual_environment_vm.ubuntu_vm[count.index].vm_id}")
+  
+  content = <<-EOF
+    # Managed by Terraform
+    # VM ID: ${proxmox_virtual_environment_vm.ubuntu_vm[count.index].vm_id}
+    # VM: ${proxmox_virtual_environment_vm.ubuntu_vm[count.index].name}
+    # Created: ${timestamp()}
+
+    Host ${proxmox_virtual_environment_vm.ubuntu_vm[count.index].name}
+        HostName ${try(proxmox_virtual_environment_vm.ubuntu_vm[count.index].ipv4_addresses[1][0], "0.0.0.0")}
+        User ${var.vm_username}
+        IdentityFile ~/.ssh/id_ed25519_vm_access
+        StrictHostKeyChecking no
+        UserKnownHostsFile /dev/null
+  EOF
+  
+  file_permission = "0600"
+}
+
+# Ansible Inventory: One file for all VMs
+resource "local_file" "ansible_inventory" {
+  depends_on = [time_sleep.wait_for_agent]
+
+  filename = "${path.module}/inventory.ini"
+  
+  content = <<-EOF
+[vms]
+${join("\n", [for vm in proxmox_virtual_environment_vm.ubuntu_vm : 
+  "${vm.name} ansible_host=${try(vm.ipv4_addresses[1][0], "0.0.0.0")}"
+])}
+
+[vms:vars]
+ansible_user=${var.vm_username}
+ansible_ssh_private_key_file=~/.ssh/id_ed25519_vm_access
+ansible_python_interpreter=/usr/bin/python3
+  EOF
+  
+  file_permission = "0644"
+}
+
+# Helping Output
+output "next_steps" {
+  value = <<-EOF
+    ✅ Created ${var.vm_count} VMs
+    
+    SSH Config files:
+    ${join("\n    ", [for i in range(var.vm_count) : "~/.ssh/config.d/terraform-vm-${proxmox_virtual_environment_vm.ubuntu_vm[i].vm_id}"])}
+    
+    Connect via:
+    ${join("\n    ", [for vm in proxmox_virtual_environment_vm.ubuntu_vm : "ssh ${vm.name}"])}
+    
+    Run Ansible:
+    cd ${path.module}
+    ansible-playbook -i inventory.ini playbook.yml
+  EOF
 }
